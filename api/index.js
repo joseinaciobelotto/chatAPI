6d1b6c22dd72da4349f91c9d8da832e2e8abdb15
zeapling <jibelotto@gmail.com>
2024-11-17 01:01:50 -0300

    semModoules

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
 import app from '../src/api';
 
+const cors = require('cors');
+
+const corsOptions = {
+  origin: true, // Permite todas as origens
+  optionsSuccessStatus: 200 // Para compatibilidade com alguns navegadores
+};
+
+app.use(cors(corsOptions));
+
 export default app;
